2070d14312827e8f2e9249a01fa9a5b7e7342ca1
mosesvk <kaumatule93@gmail.com>
2022-01-16 16:12:12 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,23 @@
-
-
+import {Container} from 'react-bootstrap'
 import Header from './components/Header';
 import Footer from './components/Footer';
 
 import './styles/index.scss';
-import './styles/bootstrap.min.scss'
+import './styles/bootstrap.min.scss';
 
 const App = () => {
   return (
     <>
-      <Header /> 
+      <Header />
       <main>
+        <Container>
+          
+        </Container>
         <h1>Cheefa's Fried Pies</h1>
       </main>
       <Footer />
     </>
   );
-}
+};
 
 export default App;
