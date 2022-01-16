8535657d97e7b728fefe9bb0e29feed65b22de34
mosesvk <kaumatule93@gmail.com>
2022-01-16 11:13:29 -0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 
 import './styles/index.scss';
+import './styles/bootstrap.min.scss'
 
 const App = () => {
   return (
