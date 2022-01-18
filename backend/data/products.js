8eeb49a8d2ec9e292625179697c368fb91d6beb7
mosesvk <kaumatule93@gmail.com>
2022-01-17 18:30:21 -0700

    Serving Products - Backend Routes

---
+++ backend/data/products.js
@@ -0,0 +1,76 @@
+const products = [
+  {
+    _id: '1',
+    name: 'the Cowboy',
+    image: '/images/cowboy.jpeg',
+    description:
+      'Beef and Mushroom',
+    category: 'savory',
+    price: 7.50,
+    countInStock: 0,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'Nutella',
+    image: '/images/nutella.jpeg',
+    description:
+      'Creamy Nutella',
+    category: 'sweet',
+    price: 2.50,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Caramel Apple',
+    image: '/images/caramel-apple.jpeg',
+    description:
+      'Caramel with fresh apple',
+    category: 'sweet',
+    price: 2.50,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Pizza Puff Peporoni',
+    image: '/images/pizza.jpeg',
+    description:
+      'New Pizza Puff',
+    category: 'pizza',
+    price: 9.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Pineapple',
+    image: '/images/pineapple.jpeg',
+    description:
+      'Fresh Pineapple',
+    category: 'sweet',
+    price: 2.50,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    _id: '6',
+    name: 'Coconut',
+    image: '/images/coconut.jpeg',
+    description:
+      'Fresh coconut',
+    category: 'sweet',
+    price: 2.50,
+    countInStock: 0,
+    rating: 4,
+    numReviews: 12,
+  },
+]
+
+module.exports = products
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -23,7 +23,7 @@ const ProductScreen = () => {
       </Link>
       <Row>
         <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid />
+          <Image className='product-img' src={product.image} alt={product.name} fluid />
         </Col>
         <Col md={3}>
           <ListGroup variant='flush'>
