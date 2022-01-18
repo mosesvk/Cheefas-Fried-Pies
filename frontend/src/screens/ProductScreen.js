83f48d50112d0c51f1bfc628d883851244c29be2
mosesvk <kaumatule93@gmail.com>
2022-01-17 18:51:10 -0700

    Fetching Products from React

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useParams } from 'react-router-dom';
+import axios from 'axios';
 import {
   Row,
   Col,
@@ -9,12 +10,22 @@ import {
   Button,
   ListGroupItem,
 } from 'react-bootstrap';
+
 import Rating from '../components/Rating';
-import products from '../products';
 
 const ProductScreen = () => {
+  const [product, setProduct] = useState([]);
   const { id } = useParams();
-  const product = products.find((p) => p._id === id);
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${id}`);
+
+      setProduct(data);
+    };
+
+    fetchProduct();
+  }, [id]);
 
   return (
     <>
@@ -23,7 +34,12 @@ const ProductScreen = () => {
       </Link>
       <Row>
         <Col md={6}>
-          <Image className='product-img' src={product.image} alt={product.name} fluid />
+          <Image
+            className='product-img'
+            src={product.image}
+            alt={product.name}
+            fluid
+          />
         </Col>
         <Col md={3}>
           <ListGroup variant='flush'>
