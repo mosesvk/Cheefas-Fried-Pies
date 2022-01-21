27710f40a7ac8f6085f004e449f9a761c6e8eb50
mosesvk <kaumatule93@gmail.com>
2022-01-21 15:46:20 -0700

    Add to Cart Functionality

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,25 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import {Link, useParams, useLocation} from 'react-router-dom'
+import {useDispatch, useSelector} from 'react-redux'
+import Message from '../components/Message'
+
+import { addToCart } from '../store/actions/cartActions'
 
 const CartScreen = () => {
+  const dispatch = useDispatch();
+  const {id} = useParams(); 
+  const location = useLocation()
+  const qty = location.search ? Number(location.search.split('=')[1]) : 1
+
+  const cart = useSelector(state => state.cart)
+  const {cartItems} = cart
+
+  useEffect(() => {
+    if (id) {
+      dispatch(addToCart(id, qty))
+    }
+  }, [dispatch, id, qty])
+
   return (
     <div>
       <h1>Cart Screen</h1>
