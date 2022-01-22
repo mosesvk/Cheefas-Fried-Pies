e31cc7dfda458d40347ef3c691f1f867326590a9
mosesvk <kaumatule93@gmail.com>
2022-01-22 16:47:49 -0700

    generate JSON Web Token

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,6 @@
 import asyncHandler from 'express-async-handler';
+import generateToken from '../utils/generateToken.js';
+
 import User from '../models/userModel.js';
 
 //@desc     Auth user & get token
@@ -9,13 +11,13 @@ const authUser = asyncHandler(async (req, res) => {
   
   const user = await User.findOne({email})
 
-  if (user && user.mathPassword(password)) {
+  if (user && (await user.matchPassword(password))) {
     res.json({
       _id: user._id,
       name: user.name,
       email:user.email,
       isAdmin: user.isAdmin,
-      token: null
+      token: generateToken(user._id)
     })
   } else {
     res.status(401)
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -23,7 +23,7 @@ const userSchema = mongoose.Schema({
   timestamps: true
 })
 
-userSchema.methods.mathPassword = async function(enteredPassword) {
+userSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password)
 }
 
