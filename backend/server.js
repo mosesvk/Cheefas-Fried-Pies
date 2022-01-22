4a93c7f65ddc81fa3b83c17c69f58bd467aabba4
mosesvk <kaumatule93@gmail.com>
2022-01-22 16:30:41 -0700

    user authentication endpoint

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import connectDB from './config/db.js';
 
 import productRoutes from './routes/productRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
 
@@ -12,11 +13,15 @@ connectDB();
 
 const app = express();
 
+//allows us to accept JSON data in the body
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
 
 app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes)
 
 //errorMiddleware.js
 app.use(notFound);
