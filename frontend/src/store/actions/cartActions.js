2a8540312d4f16125c0266170b13c91044ac4ebd
mosesvk <kaumatule93@gmail.com>
2022-01-21 20:04:08 -0700

    remove items from cart

--- frontend/src/store/actions/cartActions.js
+++ frontend/src/store/actions/cartActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { CART_ADD_ITEM } from '../constants/cartConstants';
+import { CART_ADD_ITEM, CART_REMOVE_ITEM } from '../constants/cartConstants';
 
 export const addToCart = (id, qty) => async (dispatch, getState) => {
   const { data } = await axios.get(`/api/products/${id}`);
@@ -11,9 +11,19 @@ export const addToCart = (id, qty) => async (dispatch, getState) => {
       name: data.name,
       image: data.image,
       price: data.price,
+      countInStock: data.countInStock,
       qty,
     },
   });
 
   localStorage.setItem('cartitems', JSON.stringify(getState().cart.cartItems));
 };
+
+export const removeFromCart = (id) => (dispatch, getState) => {
+  dispatch({
+    type: CART_REMOVE_ITEM,
+    payload: id
+  })
+
+  localStorage.setItem('cartitems', JSON.stringify(getState().cart.cartItems));
+}
