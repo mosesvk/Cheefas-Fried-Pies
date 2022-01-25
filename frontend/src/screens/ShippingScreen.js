4ebfd56a5fd1daa99081b606cd471a3bed4ff80d
mosesvk <kaumatule93@gmail.com>
2022-01-25 12:38:37 -0700

    sec. 9 - Shipping Screen & Save Address

--- frontend/src/screens/ShippingScreen.js
+++ frontend/src/screens/ShippingScreen.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import {useNavigate} from 'react-router-dom'
 import { Form, Button, Row, Col } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../components/FormContainer';
+import { saveShippingAddress } from '../store/actions/cartActions';
 
 const ShippingScreen = () => {
-  const [address, setAddress] = useState('');
-  const [city, setCity] = useState('');
-  const [postalCode, setPostalCode] = useState('');
-  const [country, setCountry] = useState('');
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+
+  const [address, setAddress] = useState(shippingAddress.address);
+  const [city, setCity] = useState(shippingAddress.city);
+  const [postalCode, setPostalCode] = useState(shippingAddress.postalCode);
+  const [country, setCountry] = useState(shippingAddress.country);
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch()
 
   const submitHandler = (e) => {
-    e.preventDefault()
-    console.log('submit')
-  }
+    e.preventDefault();
+    dispatch(saveShippingAddress({address, city, postalCode, country}))
+    navigate('/payment')
+  };
 
   return (
     <FormContainer>
