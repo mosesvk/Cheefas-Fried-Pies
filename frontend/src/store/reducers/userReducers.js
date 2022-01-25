06591bf2a8558238cdc4926dc45368119a6e331a
mosesvk <kaumatule93@gmail.com>
2022-01-24 18:19:29 -0700

    User Login Screen & Functionality

--- frontend/src/store/reducers/userReducers.js
+++ frontend/src/store/reducers/userReducers.js
@@ -5,17 +5,17 @@ import {
   USER_LOGOUT
 } from '../constants/userConstants'
 
-export const userLoginReducer = (state = { }, action) => {
+export const userLoginReducer = (state = {}, action) => {
   switch (action.type) {
     case USER_LOGIN_REQUEST:
-      return { loading: true };
+      return { loading: true }
     case USER_LOGIN_SUCCESS:
-      return { loading: false, userInfo: action.payload };
+      return { loading: false, userInfo: action.payload }
     case USER_LOGIN_FAIL:
-      return { loading: false, error: action.payload };
-    case USER_LOGOUT: 
+      return { loading: false, error: action.payload }
+    case USER_LOGOUT:
       return {}
     default:
-      return state;
+      return state
   }
-};
+}
