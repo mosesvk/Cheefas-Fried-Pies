838ae273a9efbee4b024ae7dfe551c3774063101
mosesvk <kaumatule93@gmail.com>
2022-01-24 18:21:27 -0700

    User Login Screen & Functionality

--- frontend/src/store/actions/userActions.js
+++ frontend/src/store/actions/userActions.js
@@ -9,26 +9,26 @@ export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST,
-    });
+    })
 
     const config = {
       headers: {
         'Content-Type': 'application/json',
       },
-    };
+    }
 
     const { data } = await axios.post(
       '/api/users/login',
       { email, password },
       config
-    );
+    )
 
     dispatch({
       type: USER_LOGIN_SUCCESS,
       payload: data,
-    });
+    })
 
-    localStorage.setItem('userInfo', JSON.stringify(data));
+    localStorage.setItem('userInfo', JSON.stringify(data))
   } catch (error) {
     dispatch({
       type: USER_LOGIN_FAIL,
@@ -36,6 +36,6 @@ export const login = (email, password) => async (dispatch) => {
         error.response && error.response.data.message
           ? error.response.data.message
           : error.message,
-    });
+    })
   }
-};
+}
