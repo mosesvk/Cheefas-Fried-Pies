dc3d0608d30319816ebc44d724249bd838e90b90
mosesvk <kaumatule93@gmail.com>
2022-01-25 19:12:07 -0700

    sec. 9 - Place Order Screen

--- frontend/src/Routes.js
+++ frontend/src/Routes.js
@@ -5,6 +5,7 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
 import PaymentScreen from './screens/PaymentScreen';
+import PlaceOrderScreen from './screens/PlaceOrderScreen';
 import ProductScreen from './screens/ProductScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import RegisterScreen from './screens/RegisterScreen';
@@ -16,6 +17,7 @@ export default (
   <Routes>
     <Route path='/shipping' element={<ShippingScreen />} />
     <Route path='/payment' element={<PaymentScreen />} />
+    <Route path='/placeorder' element={<PlaceOrderScreen />} />
     <Route path='/login' element={<LoginScreen />} />
     <Route path='/register' element={<RegisterScreen />} />
     <Route path='/profile' element={<ProfileScreen />} />
