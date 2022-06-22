4f82a4a89a079c9cd3977c9ea6f35c69407976cb
mosesvk <kaumatule93@gmail.com>
2022-06-21 19:24:36 -0600

    styling

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -150,7 +150,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9bcf07fca00dbe07"; }
+/******/ 		__webpack_require__.h = function() { return "85fce44319544ac2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Layout.jsx
+++ components/Layout.jsx
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   return (
     <div className="layout">
       <Head>
-        <title>JS Mastery Store</title>
+        <title>Cheefa's Fried Pies</title>
       </Head>
       <header>
         <Navbar />
